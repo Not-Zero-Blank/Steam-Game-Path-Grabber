906e358f8ffe9cf0a567a44f83f3ead81fed4f66
⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ <JanTV720@gmail.com>
2022-02-24 13:51:08 +0100

    Updated to Steam General Games

--- Program.cs
+++ Program.cs
@@ -2,34 +2,28 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-internal class Program
+public class SteamPathGrabber
 {
-    static void Main(string[] args)
+    public static void Grab(string SteamGameToGrab)
     {
         try
         {
             string Raw = File.ReadAllText(GetSteamPath() + @"\steamapps\libraryfolders.vdf");
             int count = -1;
-            bool result = false;
-            string VRCPath = string.Empty;
             foreach (string a in Raw.Split(new string[] { "\"path\"" }, StringSplitOptions.None))
             {
-                if (result) continue;
                 if (count == -1)
                 {
 
                 }
                 else
                 {
-                    Console.WriteLine($"Got Library {count} Path: {a.Split('"')[1]}\nChecking For VRC...");
+                    Console.WriteLine($"Got Library {count} Path: {a.Split('"')[1]}\nChecking for {SteamGameToGrab}...");
                     foreach (string b in GetAppsPath(a.Split('"')[1]))
                     {
-                        if (result) continue;
-                        if (b.Contains("VRChat"))
+                        if (b.Contains(SteamGameToGrab))
                         {
-                            Console.WriteLine(b);
-                            VRCPath = b;
-                            result = true;
+                            return b;
                         }
                     }
                 }
@@ -44,7 +38,7 @@ internal class Program
     }
     static string GetSteamPath()
     {
-        object a = Registry.CurrentUser.OpenSubKey(@"Software\Valve\Steam").GetValue("SteamPath");
+        object a = Registry.CurrentUser.OpenSubKey(@"Software\Valve\Steam").GetValue("SteamPath"); //Just Reading doesnt require Admin
         if (a == null) throw new Exception("Cant find Steam!");
         return a.ToString();
     }
